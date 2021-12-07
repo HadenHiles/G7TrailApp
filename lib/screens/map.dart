3e6497dd732beb1cf4540832d0ba799dc221f50f
HadenHiles <hadenhiles@gmail.com>
2021-12-06 23:53:03 -0500

    Add static map image to map tab temporarily

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:g7trailapp/widgets/screen_title.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
 
   @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
-        Text("Map"),
-      ],
+    return NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return [
+          SliverAppBar(
+            collapsedHeight: 65,
+            expandedHeight: 100,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            iconTheme: Theme.of(context).iconTheme,
+            actionsIconTheme: Theme.of(context).iconTheme,
+            floating: true,
+            pinned: false,
+            flexibleSpace: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+              ),
+              child: FlexibleSpaceBar(
+                collapseMode: CollapseMode.parallax,
+                titlePadding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                centerTitle: false,
+                title: ScreenTitle(icon: Icons.map_rounded, title: "Trail Map"),
+                background: Container(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+              ),
+            ),
+          ),
+        ];
+      },
+      body: Expanded(
+        child: FittedBox(
+          fit: BoxFit.cover,
+          child: Image(
+            image: AssetImage("assets/images/temp-map.jpg"),
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
