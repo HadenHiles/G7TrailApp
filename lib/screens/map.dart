70b9766bb857ca3e1c80b0555f10b223347a9435
HadenHiles <hadenhiles@gmail.com>
2022-01-09 20:08:41 -0500

    Remove headerSliver from map screen so that the map can safely take up the area above bottom navigation and utilize as much screen as possible

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:g7trailapp/widgets/screen_title.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -21,35 +19,8 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-            collapsedHeight: 65,
-            expandedHeight: 100,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            iconTheme: Theme.of(context).iconTheme,
-            actionsIconTheme: Theme.of(context).iconTheme,
-            floating: true,
-            pinned: false,
-            flexibleSpace: DecoratedBox(
-              decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-              ),
-              child: FlexibleSpaceBar(
-                collapseMode: CollapseMode.parallax,
-                titlePadding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-                centerTitle: false,
-                title: ScreenTitle(icon: Icons.map_rounded, title: "Trail Map"),
-                background: Container(
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                ),
-              ),
-            ),
-          ),
-        ];
-      },
-      body: GoogleMap(
+    return SafeArea(
+      child: GoogleMap(
         mapType: MapType.terrain,
         compassEnabled: true,
         myLocationEnabled: true,
