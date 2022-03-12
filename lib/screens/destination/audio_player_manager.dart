d888debc911e31c5bf22c102ea086f20c4e2bc17
HadenHiles <hadenhiles@gmail.com>
2022-03-11 20:55:55 -0500

    Add audio player progress bar and play button to destination UI

---
+++ lib/screens/destination/audio_player_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AudioPlayerManager {
+  final buttonNotifier = ValueNotifier<ButtonState>(ButtonState.paused);
+  final progressNotifier = ValueNotifier<ProgressBarState>(
+    ProgressBarState(
+      current: Duration.zero,
+      buffered: Duration.zero,
+      total: Duration.zero,
+    ),
+  );
+}
+
+class ProgressBarState {
+  ProgressBarState({
+    required this.current,
+    required this.buffered,
+    required this.total,
+  });
+
+  final Duration current;
+  final Duration buffered;
+  final Duration total;
+}
+
+enum ButtonState { paused, playing, loading }
