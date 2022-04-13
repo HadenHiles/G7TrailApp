b8a16d1d29aec551ffe03f7bfc9d87db27ec507e
hadenhiles <hi@hadenhiles.com>
2022-04-13 11:59:03 -0400

    Setup logic for saving hike data for logged in user. (show dialog if user isn't logged in and trying to save.

---
+++ lib/models/firestore/hike.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Hike {
+  String? id;
+  String data;
+  Duration duration;
+  DateTime date;
+  DocumentReference? reference;
+
+  Hike(this.data, this.duration, this.date);
+
+  Hike.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['data'] != null),
+        assert(map['duration'] != null),
+        assert(map['date'] != null),
+        id = map['id'],
+        data = map['data'],
+        duration = Duration(seconds: map['duration']),
+        date = map['date'].toDate();
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'data': data,
+      'duration': duration.inSeconds,
+      'date': date,
+    };
+  }
+
+  Hike.fromSnapshot(DocumentSnapshot? snapshot) : this.fromMap(snapshot!.data() as Map<String, dynamic>, reference: snapshot.reference);
+}
