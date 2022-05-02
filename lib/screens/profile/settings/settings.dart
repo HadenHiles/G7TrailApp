6875f5ee457463146432c0aebf2fbcafa013cf6b
hadenhiles <hi@hadenhiles.com>
2022-05-02 10:32:12 -0400

    Make sure profile screen reloads properly after logging out

--- lib/screens/profile/settings/settings.dart
+++ lib/screens/profile/settings/settings.dart
@@ -211,7 +211,6 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                                 onPressed: (BuildContext context) {
                                   signOut();
 
-                                  navigatorKey.currentState!.pop();
                                   navigatorKey.currentState!.pushReplacement(
                                     MaterialPageRoute(
                                       builder: (context) {
