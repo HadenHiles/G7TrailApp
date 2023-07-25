3671897f390836ce1dfabb1c9207bc61897091d0
HadenHiles <hadenhiles@gmail.com>
2023-07-25 08:12:21 -0400

    WIP: Update flutter and pub packages to latest versions - start replacing deprecated packages with alternatives

--- lib/services/network_status_service.dart
+++ lib/services/network_status_service.dart
@@ -1,20 +1,24 @@
 // ignore_for_file: constant_identifier_names
 
 import 'dart:async';
-import 'package:ci_connectivity/ci_connectivity.dart';
+import 'package:ac_inet_connectivity_checker/ac_inet_connectivity_checker.dart';
 
 enum NetworkStatus { Online, Offline }
 
 class NetworkStatusService {
-  final ciConnectivity = CiConnectivity();
-
   StreamController<NetworkStatus> networkStatusController = StreamController<NetworkStatus>();
 
   NetworkStatusService() {
     // actively listen for status updates
-    ciConnectivity.loopVerifyStatus();
-    ciConnectivity.onListenerStatusNetwork.listen((event) {
-      switch (event) {
+    final checker = InetConnectivityChecker(
+      endpoint: InetEndpoint(host: 'google.com', port: 443),
+      // optional timeout. if unspecified the operating system
+      // default connect timeout will be used instead (~120s).
+      timeout: const Duration(seconds: 10),
+    );
+
+    checker.cancelableOperation.value.then((connected) {
+      switch (connected) {
         case true:
           networkStatusController.add(NetworkStatus.Online);
           break;
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,112 +4,111 @@ import 'package:flutter_html/style.dart';
 class HomeTheme {
   HomeTheme._();
 
-  static final Map<String, Style> lightHtmlStyle = {
+  static final Map<String, dynamic> lightHtmlStyle = {
     'h1': Style(
-      color: lightTheme.textTheme.headline1!.color,
-      fontFamily: lightTheme.textTheme.headline1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline1!.fontSize ?? 42, Unit.px),
+      color: lightTheme.textTheme.displayLarge!.color,
+      fontFamily: lightTheme.textTheme.displayLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displayLarge!.fontSize ?? 42, Unit.px),
       textTransform: TextTransform.uppercase,
     ),
     'h2': Style(
-      color: lightTheme.textTheme.headline2!.color,
-      fontFamily: lightTheme.textTheme.headline2!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline2!.fontSize ?? 36, Unit.px),
+      color: lightTheme.textTheme.displayMedium!.color,
+      fontFamily: lightTheme.textTheme.displayMedium!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displayMedium!.fontSize ?? 36, Unit.px),
       textTransform: TextTransform.uppercase,
     ),
     'h3': Style(
-      color: lightTheme.textTheme.headline3!.color,
-      fontFamily: lightTheme.textTheme.headline3!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline3!.fontSize ?? 30, Unit.px),
+      color: lightTheme.textTheme.displaySmall!.color,
+      fontFamily: lightTheme.textTheme.displaySmall!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displaySmall!.fontSize ?? 30, Unit.px),
     ),
     'h4': Style(
-      color: lightTheme.textTheme.headline4!.color,
-      fontFamily: lightTheme.textTheme.headline4!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline4!.fontSize ?? 24, Unit.px),
+      color: lightTheme.textTheme.headlineMedium!.color,
+      fontFamily: lightTheme.textTheme.headlineMedium!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.headlineMedium!.fontSize ?? 24, Unit.px),
     ),
     'h5': Style(
-      color: lightTheme.textTheme.headline5!.color,
-      fontFamily: lightTheme.textTheme.headline5!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline5!.fontSize ?? 22, Unit.px),
+      color: lightTheme.textTheme.headlineSmall!.color,
+      fontFamily: lightTheme.textTheme.headlineSmall!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.headlineSmall!.fontSize ?? 22, Unit.px),
     ),
     'h6': Style(
-      color: lightTheme.textTheme.headline6!.color,
-      fontFamily: lightTheme.textTheme.headline6!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline6!.fontSize ?? 18, Unit.px),
+      color: lightTheme.textTheme.titleLarge!.color,
+      fontFamily: lightTheme.textTheme.titleLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.titleLarge!.fontSize ?? 18, Unit.px),
     ),
     'p': Style(
-      color: lightTheme.textTheme.bodyText1!.color,
-      fontFamily: lightTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: lightTheme.textTheme.bodyLarge!.color,
+      fontFamily: lightTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
       lineHeight: LineHeight(1.75, units: 'em'),
       padding: EdgeInsets.symmetric(vertical: 5),
     ),
     'em': Style(
-      color: lightTheme.textTheme.bodyText1!.color,
-      fontFamily: lightTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: lightTheme.textTheme.bodyLarge!.color,
+      fontFamily: lightTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
       fontStyle: FontStyle.italic,
     ),
     'strong': Style(
-      color: lightTheme.textTheme.bodyText1!.color,
-      fontFamily: lightTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: lightTheme.textTheme.bodyLarge!.color,
+      fontFamily: lightTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
       fontWeight: FontWeight.bold,
     ),
   };
-  static final Map<String, Style> darkHtmlStyle = {
+  static final Map<String, dynamic> darkHtmlStyle = {
     'h1': Style(
-      color: darkTheme.textTheme.headline1!.color,
-      fontFamily: darkTheme.textTheme.headline1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline1!.fontSize ?? 42, Unit.px),
+      color: darkTheme.textTheme.displayLarge!.color,
+      fontFamily: darkTheme.textTheme.displayLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displayLarge!.fontSize ?? 42, Unit.px),
     ),
     'h2': Style(
-      color: darkTheme.textTheme.headline2!.color,
-      fontFamily: darkTheme.textTheme.headline2!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline2!.fontSize ?? 36, Unit.px),
+      color: darkTheme.textTheme.displayMedium!.color,
+      fontFamily: darkTheme.textTheme.displayMedium!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displayMedium!.fontSize ?? 36, Unit.px),
     ),
     'h3': Style(
-      color: darkTheme.textTheme.headline3!.color,
-      fontFamily: darkTheme.textTheme.headline3!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline3!.fontSize ?? 30, Unit.px),
+      color: darkTheme.textTheme.displaySmall!.color,
+      fontFamily: darkTheme.textTheme.displaySmall!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.displaySmall!.fontSize ?? 30, Unit.px),
     ),
     'h4': Style(
-      color: darkTheme.textTheme.headline4!.color,
-      fontFamily: darkTheme.textTheme.headline4!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline4!.fontSize ?? 24, Unit.px),
+      color: darkTheme.textTheme.headlineMedium!.color,
+      fontFamily: darkTheme.textTheme.headlineMedium!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.headlineMedium!.fontSize ?? 24, Unit.px),
     ),
     'h5': Style(
-      color: darkTheme.textTheme.headline5!.color,
-      fontFamily: darkTheme.textTheme.headline5!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline5!.fontSize ?? 22, Unit.px),
+      color: darkTheme.textTheme.headlineSmall!.color,
+      fontFamily: darkTheme.textTheme.headlineSmall!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.headlineSmall!.fontSize ?? 22, Unit.px),
     ),
     'h6': Style(
-      color: darkTheme.textTheme.headline6!.color,
-      fontFamily: darkTheme.textTheme.headline6!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.headline6!.fontSize ?? 18, Unit.px),
+      color: darkTheme.textTheme.titleLarge!.color,
+      fontFamily: darkTheme.textTheme.titleLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.titleLarge!.fontSize ?? 18, Unit.px),
     ),
     'p': Style(
-      color: darkTheme.textTheme.bodyText1!.color,
-      fontFamily: darkTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: darkTheme.textTheme.bodyLarge!.color,
+      fontFamily: darkTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
     ),
     'em': Style(
-      color: darkTheme.textTheme.bodyText1!.color,
-      fontFamily: darkTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: darkTheme.textTheme.bodyLarge!.color,
+      fontFamily: darkTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
       fontStyle: FontStyle.italic,
     ),
     'strong': Style(
-      color: darkTheme.textTheme.bodyText1!.color,
-      fontFamily: darkTheme.textTheme.bodyText1!.fontFamily,
-      fontSize: FontSize(darkTheme.textTheme.bodyText1!.fontSize ?? 16, Unit.px),
+      color: darkTheme.textTheme.bodyLarge!.color,
+      fontFamily: darkTheme.textTheme.bodyLarge!.fontFamily,
+      fontSize: FontSize(darkTheme.textTheme.bodyLarge!.fontSize ?? 16, Unit.px),
       fontWeight: FontWeight.bold,
     ),
   };
 
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    backgroundColor: Color(0xffF2F2F2),
     primaryColor: Color(0xff0053B1),
     scaffoldBackgroundColor: Color(0xffE5E5E5),
     appBarTheme: AppBarTheme(
@@ -118,7 +117,91 @@ class HomeTheme {
         color: Colors.white,
       ),
     ),
-    toggleableActiveColor: Color(0xff0053B1),
+    cardTheme: CardTheme(
+      color: Color(0xffF2F2F2),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.black87,
+    ),
+    textTheme: TextTheme(
+      displayLarge: TextStyle(
+        fontSize: 42,
+        fontFamily: 'LGCafe',
+        color: Colors.black87,
+      ),
+      displayMedium: TextStyle(
+        fontSize: 36,
+        color: Colors.black87,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 30,
+        color: Colors.black87,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 24,
+        color: Colors.black87,
+      ),
+      headlineSmall: TextStyle(
+        color: Colors.black87,
+        fontFamily: 'LGCafe',
+        fontSize: 22,
+      ),
+      titleLarge: TextStyle(
+        color: Color(0xff0053B1),
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyLarge: TextStyle(
+        color: Colors.black54,
+        fontSize: 16,
+      ),
+      bodyMedium: TextStyle(
+        color: Colors.black54,
+        fontSize: 14,
+      ),
+    ),
+    checkboxTheme: CheckboxThemeData(
+      fillColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
+    radioTheme: RadioThemeData(
+      fillColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
+    switchTheme: SwitchThemeData(
+      thumbColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+      trackColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
     colorScheme: ColorScheme.light(
       brightness: Brightness.light,
       primary: Color(0xffF2F2F2),
@@ -127,55 +210,11 @@ class HomeTheme {
       secondary: Color(0xffA9B7A7),
       onSecondary: Colors.white,
       onBackground: Colors.black,
-    ),
-    cardTheme: CardTheme(
-      color: Color(0xffF2F2F2),
-    ),
-    iconTheme: IconThemeData(
-      color: Colors.black87,
-    ),
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        fontSize: 42,
-        fontFamily: 'LGCafe',
-        color: Colors.black87,
-      ),
-      headline2: TextStyle(
-        fontSize: 36,
-        color: Colors.black87,
-      ),
-      headline3: TextStyle(
-        fontSize: 30,
-        color: Colors.black87,
-      ),
-      headline4: TextStyle(
-        fontSize: 24,
-        color: Colors.black87,
-      ),
-      headline5: TextStyle(
-        color: Colors.black87,
-        fontFamily: 'LGCafe',
-        fontSize: 22,
-      ),
-      headline6: TextStyle(
-        color: Color(0xff0053B1),
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),
-      bodyText1: TextStyle(
-        color: Colors.black54,
-        fontSize: 16,
-      ),
-      bodyText2: TextStyle(
-        color: Colors.black54,
-        fontSize: 14,
-      ),
-    ),
+    ).copyWith(background: Color(0xffF2F2F2)),
   );
 
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    backgroundColor: Color(0xff222222),
     primaryColor: Color(0xff0053B1),
     scaffoldBackgroundColor: Color(0xff1A1A1A),
     appBarTheme: AppBarTheme(
@@ -184,7 +223,90 @@ class HomeTheme {
         color: Colors.white,
       ),
     ),
-    toggleableActiveColor: Color(0xff0053B1),
+    cardTheme: CardTheme(
+      color: Color(0xff333333),
+    ),
+    iconTheme: IconThemeData(
+      color: Color.fromRGBO(255, 255, 255, 0.8),
+    ),
+    textTheme: TextTheme(
+      displayLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 42,
+      ),
+      displayMedium: TextStyle(
+        color: Colors.white,
+        fontSize: 36,
+      ),
+      displaySmall: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.8),
+        fontSize: 30,
+      ),
+      headlineMedium: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.8),
+        fontSize: 24,
+      ),
+      headlineSmall: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.8),
+        fontFamily: 'LGCafe',
+        fontSize: 22,
+      ),
+      titleLarge: TextStyle(
+        color: Color(0xff0053B1),
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyLarge: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.8),
+        fontSize: 16,
+      ),
+      bodyMedium: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.8),
+        fontSize: 14,
+      ),
+    ),
+    checkboxTheme: CheckboxThemeData(
+      fillColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
+    radioTheme: RadioThemeData(
+      fillColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
+    switchTheme: SwitchThemeData(
+      thumbColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+      trackColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.disabled)) {
+          return null;
+        }
+        if (states.contains(MaterialState.selected)) {
+          return Color(0xff0053B1);
+        }
+        return null;
+      }),
+    ),
     colorScheme: ColorScheme.dark(
       brightness: Brightness.dark,
       primary: Color(0xff1A1A1A),
@@ -193,49 +315,7 @@ class HomeTheme {
       secondary: Color(0xffA9B7A7),
       onSecondary: Colors.white,
       onBackground: Colors.white,
-    ),
-    cardTheme: CardTheme(
-      color: Color(0xff333333),
-    ),
-    iconTheme: IconThemeData(
-      color: Color.fromRGBO(255, 255, 255, 0.8),
-    ),
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        color: Colors.white,
-        fontSize: 42,
-      ),
-      headline2: TextStyle(
-        color: Colors.white,
-        fontSize: 36,
-      ),
-      headline3: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 0.8),
-        fontSize: 30,
-      ),
-      headline4: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 0.8),
-        fontSize: 24,
-      ),
-      headline5: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 0.8),
-        fontFamily: 'LGCafe',
-        fontSize: 22,
-      ),
-      headline6: TextStyle(
-        color: Color(0xff0053B1),
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),
-      bodyText1: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 0.8),
-        fontSize: 16,
-      ),
-      bodyText2: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 0.8),
-        fontSize: 14,
-      ),
-    ),
+    ).copyWith(background: Color(0xff222222)),
   );
 }
 
