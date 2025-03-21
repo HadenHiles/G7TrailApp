b67f6de37559217ef165d2a7bd2c2d0705afac75
Haden Hiles <hi@hadenhiles.com>
2025-03-20 20:51:21 -0400

    Attempt to set google maps api key for iOS use
    WIP: Fix app crash when attempting to open map tab.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,16 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    var keys: NSDictionary?
+
+    if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
+        keys = NSDictionary(contentsOfFile: path)
+    }
+    if let dict = keys {
+        let googleMapsAPIKey = dict["googleMapsAPIKey"] as? String
+        // Google maps api key for iOS use
+        GMSServices.provideAPIKey(googleMapsAPIKey!)
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
